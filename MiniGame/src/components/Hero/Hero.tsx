96eaef12fcc5a8d98c20e4499f1879a73bda96d4
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-21 21:06:08 +0200

    Update Hero.tsx

--- MiniGame/src/components/Hero/Hero.tsx
+++ MiniGame/src/components/Hero/Hero.tsx
@@ -1,10 +1,11 @@
 import { Container, Sprite, useTick } from "@pixi/react"
 import { Texture } from "pixi.js"
 import { useCallback, useEffect, useRef } from "react"
-import { DEFAULT_POS_X, DEFAULT_POS_Y, MOVE_SPEED } from "../../constants/game-world"
+import { ANIMATION_SPEED, DEFAULT_POS_X, DEFAULT_POS_Y, MOVE_SPEED } from "../../constants/game-world"
 import { useHeroControls } from "./useHeroControls"
 import { Direction, IPosition } from "../../types/common"
 import { calculateNewTarget, checkCanMove, handleMovement } from "../../helpers/common"
+import { useHeroAnimation } from "./useHeroAnimation"
 
 interface IHeroProps {
     texture: Texture
@@ -16,8 +17,15 @@ export const Hero = ({texture, onMove}: IHeroProps) => {
     const targetPosition = useRef<IPosition | null>(null)
     const currentDirection = useRef<Direction | null>(null)
     const {getControlsDirection} = useHeroControls()
-
     const direction = getControlsDirection()
+    const isMoving = useRef(false)
+    const { sprite, updateSprite} = useHeroAnimation({
+        texture,
+        frameHeight: 64,
+        frameWidth: 64,
+        totalFrames: 9,
+        animationSpeed: ANIMATION_SPEED
+    })
     
 
     useEffect(()=> {
@@ -48,22 +56,25 @@ export const Hero = ({texture, onMove}: IHeroProps) => {
         const { completed, position: newPosition} = handleMovement(position.current, targetPosition.current, MOVE_SPEED, delta)
         
         position.current = newPosition
-
+        isMoving.current = true
         if(completed) {
+            const { x, y} = position.current
+            onMove(x, y)
             targetPosition.current = null
+            isMoving.current = false  
         }
     }
+
+      updateSprite(currentDirection.current!, isMoving.current)
     })
     return (
         <Container>
-            <Sprite
-            texture={texture}
+            {sprite && (<Sprite
+            texture={sprite.texture}
             x={position.current.x}
             y={position.current.y}
             scale={0.5}
-            anchor={[1, 0.5]}
-            
-            />
+            anchor={[1, 0.8]}/>)}
         </Container>
     )
 }
